4840d4db5a7dd9abea4a2b0602daa066e470d878
raghavnyati <raghavnyati90@gmail.com>
2018-02-19 15:38:38 -0800

    itertools combinations

--- PrintPractice.py
+++ PrintPractice.py
@@ -2,6 +2,15 @@ def print_xxx(n):
     return print(*range(1, n+1), sep='')
 
 
+def print_combinations(str_input):
+    # input in format "HACK 2"
+    from itertools import combinations
+    s, n = str_input.split()
+    for i in range(1, int(n)+1):
+        for j in combinations(sorted(s), i):
+            print(''.join(j))
+
+
 if __name__ == '__main__':
     number = int(input())
     for x in range(1, number+1):

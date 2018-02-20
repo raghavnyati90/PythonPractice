81983df99d1321e2e2542b2cf1f993aff3603df5
raghavnyati <raghavnyati90@gmail.com>
2018-02-19 16:05:22 -0800

    itertools groupby

--- Utility.py
+++ Utility.py
@@ -1,3 +1,14 @@
 def is_leap(year):
     return (year % 4 == 0) and ((year % 400 == 0) or (year % 100 != 0))
 
+
+def compress_string(str_input):
+    """
+    You are given a string.
+    Suppose a character '' occurs consecutively  times in the string.
+    Replace these consecutive occurrences of the character '' with  in the string.
+    """
+    from itertools import groupby
+    print(*[(len(list(c)), int(k)) for k, c in groupby(str_input)])
+
+
